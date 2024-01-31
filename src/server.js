81d4c56ceca05d9f229915823a73f03ee6d14007
markdave123-py <nwaekwudavid@gmail.com>
2024-01-31 17:46:06 +0100

    Chat system- html and just server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+app.use(express.static(path.join(__dirname, "public")))
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('New WB connected');
+
+    socket.on('message', (data)=> {
+        console.log("messaging user",data)
+        io.emit('message', data);
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
